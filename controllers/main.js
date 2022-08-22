de16b557a8011146e4e61572217d5bc9fb87a814
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-22 01:08:33 -0400

    Validate USername and Password

--- controllers/main.js
+++ controllers/main.js
@@ -1,4 +1,21 @@
+// check username, password in post(login) request
+// if exist create new JWT
+// send back to from-end
+
+// setup authentication so only the request with JWT can access the dashboard
+
+const CustomAPIError = require('../errors/custom-error');
+
 const login = async (req, res) => {
+  const { username, password } = req.body;
+  // opciones de validación
+  // mongoose validation
+  // Joi package
+  // check in the controller
+  if (!username || !password) {
+    throw new CustomAPIError('Please provide email and password', 400);
+  }
+  console.log(username, password);
   res.send('Fake Login/Register/Signup');
 };
 
